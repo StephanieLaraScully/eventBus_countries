0025b015c2bb1fdf7caba4ef08ef107d1a82737f
Steph Scully <stephanielarascullion@yahoo.com>
2019-06-26 16:33:57 +0100

    MVP smashed

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div>
+    <list-item v-for="(country, index) in countries" :country="country" :key="index"></list-item>
+  </div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue';
+
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  components: {
+    "list-item": ListItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
